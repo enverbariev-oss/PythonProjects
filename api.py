9f0fcdb6eab4ecfbc435f4a689a6a5f8883f7463
Enver <enverbariev73@gmail.com>
2025-11-19 01:01:30 +0300

    аннотации и валидация

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI,HTTPException
-from typing import Optional,List,Dict
-from pydantic import BaseModel
+from idlelib.query import Query
+
+from fastapi import FastAPI,HTTPException,Path,Query,Body
+from typing import Optional,List,Dict,Annotated
+from pydantic import BaseModel,Field
 
 
 app =FastAPI()
@@ -19,11 +21,15 @@ class PostCreate(BaseModel):
     title:str
     body:str
     author_id:int
-
+class UserCreate(BaseModel):
+    name:Annotated[
+        str,Field(...,title="Имя пользователя",min_length=3,max_length=20)
+    ]
+    age:Annotated[int,Field(...,title="возраст пользователя",ge=1,lt=120)]
 users =[
     {'id':1,'name':'John','age':34},
-{'id':2,'name':'Alex','age':12},
-{'id':3,'name':'Bob','age':45},
+    {'id':2,'name':'Alex','age':12},
+    {'id':3,'name':'Bob','age':45},
 ]
 
 posts =[
@@ -52,6 +58,19 @@ async def add_item(post:PostCreate) ->Post:
     posts.append(new_post)
 
     return Post(**new_post)
+@app.post("/user/add")
+async def add_item(user:Annotated[
+    UserCreate,
+    Body(...,example={
+         "name":"Username",
+         "age":1})
+]) ->User:
+
+    new_user_id = len(users)+1
+    new_user ={'id':new_user_id,'name':user.name,'age':user.age}
+    posts.append(new_user)
+
+    return User(**new_user)
 
 
 @app.put("/items/edit/{id}")
@@ -79,14 +98,17 @@ async def add_item(post: PostCreate) -> Post:
 
 
 @app.get("/items/{id}")
-async def items(id:int)->Post:
+async def items(id:Annotated[int,Path(...,title = "Здесь указывается id поста",ge=1, lt=100)])->Post:
     for post in posts:
         if post['id'] == id:
             return Post(**post)
 
     raise HTTPException(status_code=404,detail="Post not found")
 @app.get("/search")
-async def search(post_id:Optional[int] = None) ->Dict[str,Optional[Post]]:
+async def search(post_id:Annotated[
+    Optional[int],
+    Query(title ="id of post to search for",ge=1,lt=50)
+]) ->Dict[str,Optional[Post]]:
     if post_id:
         for post in posts:
             if post['id'] == post_id:
